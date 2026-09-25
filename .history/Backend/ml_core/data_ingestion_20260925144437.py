d7ad15cdd4654a444c9e8f5116fe0ee27d815a2e
Adeku12345 <josh4global@gmail.com>
2026-09-25 14:57:49 +0100

    data transformation added

--- .history/Backend/ml_core/data_ingestion_20260925144437.py
+++ .history/Backend/ml_core/data_ingestion_20260925144437.py
@@ -7,8 +7,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from backend.ml_core.data_transformation import DataTransformation
-from backend.ml_core.data_transformation import DataTransformationConfig
+from backend
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts',"train.csv")
@@ -52,7 +51,4 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion() 
-    
-    data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)                       
+    obj.initiate_data_ingestion()                        
--- .history/Backend/ml_core/data_ingestion_20260925144629.py
+++ .history/Backend/ml_core/data_ingestion_20260925144629.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from backend.ml_core.data_transformation import DataTransformation
-from backend.ml_core.data_transformation import DataTransformationConfig
+from backend.ml_core.data_transformation import Data
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts',"train.csv")
@@ -52,7 +52,4 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion() 
-    
-    data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)                       
+    obj.initiate_data_ingestion()                        
